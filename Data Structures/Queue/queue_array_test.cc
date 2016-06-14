590a8358da9ae2895db2e74229216dd59ad3862b
Gao Xiang <gaoxiangnumber1@126.com>
2016-06-15 00:17:53 +0800

    finish queue_array_test.cc

---
+++ Data Structures/Queue/queue_array_test.cc
@@ -0,0 +1,36 @@
+#include<iostream>
+#include"Queue_Array.h"
+using namespace std;
+
+int main()
+{
+	AllOperation();
+
+	return 0;
+}
+
+/*
+1
+5 3
+1 2 3
+2
+3
+4
+5
+6 4
+6 5
+6 6
+3
+7
+6 6
+7
+6 7
+3
+7 7 7 7 7
+2
+8
+6 0
+8
+9
+0
+*/
