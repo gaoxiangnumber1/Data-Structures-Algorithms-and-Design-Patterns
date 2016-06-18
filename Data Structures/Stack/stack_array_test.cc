9e54394e8c14d1ac29df49444ad34e76605a55a5
Gao Xiang <gaoxiangnumber1@126.com>
2016-06-19 00:09:26 +0800

    finish stack_array_test.cc

---
+++ Data Structures/Stack/stack_array_test.cc
@@ -0,0 +1,36 @@
+#include<iostream>
+#include"Stack_Array.h"
+using namespace std;
+
+int main()
+{
+	AllOperation();
+
+	return 0;
+}
+
+/*
+1
+5 2
+1 2
+2
+3
+4
+5 3
+5 4
+5 5
+5 6
+3
+4
+6 6 6 6 6
+2
+3
+4
+7
+5 0
+8
+1 5
+1 2 3 4 5
+7
+0
+*/
